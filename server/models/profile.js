90906035f376d5796b87b5cce72be82a9747aace
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-07 04:35:03 +0530

    Update models

--- server/models/profile.js
+++ server/models/profile.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: "string", required: true },
+  email: { type: String, required: true },
   hobbies: { type: [String] },
+
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const Profile = mongoose.model("Profile", profileSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: "string", required: true },
   username: { type: String, required: true, unique: true },
-  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 
   profileId: { type: mongoose.Schema.Types.ObjectId, ref: "Profile" },
