b84e0c8f84d495bf04b1d894d7ef446fa18cb322
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-07 04:35:42 +0530

    get user profile

--- server/routers/profile.js
+++ server/routers/profile.js
@@ -1,20 +1,37 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+
+// jwt authentication
+const verify = require("../util/verify");
 
 // model
 const { User } = require("../models/user");
 const { Profile } = require("../models/profile");
 
 // GET user and profile
-router.get("/:username", (req, res) => {
+router.get("/:username", verify, (req, res) => {
   const username = req.params.username;
+  let permissions = false; // true if token matches with the user
+
+  //   finding user with the given username
   User.findOne({ username }, (err, user) => {
-    if (err) {
-      res.status(404).send({ status: "Error", message: err.message });
+    if (err)
+      return res.status(404).send({ status: "Error", message: err.message });
+
+    if (!user) {
+      res.status(404).send({ status: "Error", message: "User not found" });
     } else {
       const userId = user._id;
+      if (userId && userId == req.user?.id) {
+        permissions = true;
+      }
+      // getting profile
       Profile.findOne({ userId }, (err, profile) => {
-        res.status(200).send({ status: "OK", user, profile });
+        if (err)
+          return res
+            .status(404)
+            .send({ status: "Error", message: "User profile not found!" });
+
+        res.status(200).send({ status: "OK", data: { permissions, profile } });
       });
     }
   });
