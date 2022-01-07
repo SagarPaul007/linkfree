e70f93218131322754e05e750c76407c54987cfd
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-07 05:51:11 +0530

    updated jwt auth

--- server/util/verify.js
+++ server/util/verify.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const JWT = require("./variable");
 
-function verify(req, res, next) {
+function profileVerify(req, res, next) {
   const token = req.header("auth-token");
   if (token) {
     try {
@@ -16,4 +16,18 @@ function verify(req, res, next) {
   }
 }
 
-module.exports = verify;
+function userVerify(req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) {
+    return res.status(401).send({ status: "Error", message: "Access denied" });
+  }
+  try {
+    const verified = jwt.verify(token, JWT);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send({ status: "Error", message: err.message });
+  }
+}
+
+module.exports = { profileVerify, userVerify };
