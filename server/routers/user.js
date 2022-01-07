52596c832af632e2c9dc9fed7bfb4f3ccc99418c
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-07 05:52:01 +0530

    user update route

--- server/routers/user.js
+++ server/routers/user.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// secret key
+// secret key and jwt authentication
 const JWT = require("../util/variable");
+const { userVerify } = require("../util/verify");
 
 // user model
 const { User } = require("../models/user");
@@ -11,7 +12,7 @@ const { Profile } = require("../models/profile");
 
 // register user and create a profile
 
-router.post("/api/register", async (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password, name, email } = req.body;
   const hashedPassword = await bcrypt.hash(password, 8);
 
@@ -35,26 +36,46 @@ router.post("/api/register", async (req, res) => {
 
 // login
 
-router.post("/api/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username }).lean();
   if (!user)
     return res.send({
       status: "Error",
-      message: "Invalid usename or password",
+      message: "Invalid usename",
     });
   const match = await bcrypt.compare(password, user.password);
   if (match) {
-    const token = jwt.sign({ id: user._id, username: user.username }, JWT);
+    const token = jwt.sign({ id: user._id }, JWT);
     res.send({ status: "Success", message: "Logged in successfully", token });
   } else {
-    res.send({ status: "Error", message: "Invalid usename or password" });
+    res.send({ status: "Error", message: "Invalid password" });
   }
 });
 
 // update user (jwt verification required)
 
-router.post("/api/update", (req, res) => {});
+router.post("/update", userVerify, async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  try {
+    const userId = req.user?.id;
+    const user = await User.findOne({ _id: userId });
+    if (!user) return res.send({ status: "Error", message: "no user" });
+
+    // updating data
+    user.username = username;
+    user.password = hashedPassword;
+    await user.save();
+
+    res
+      .status(200)
+      .send({ status: "Ok", message: "user updated successfully" });
+  } catch (err) {
+    res.send({ status: "Error", message: err.message });
+  }
+});
 
 // delete user and profile (jwt verification required)
 
