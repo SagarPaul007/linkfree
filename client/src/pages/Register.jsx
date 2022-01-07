1ed7684937e947b29cf7f114e5f22b8e383c606a
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-08 04:51:56 +0530

    registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,8 +2,31 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register">
+      <form>
+        <div className="form-heading">
+          <h2>Register User</h2>
+        </div>
+        <div className="form-control">
+          <label for="name">Name</label>
+          <input type="text" required name="name" />
+        </div>
+        <div className="form-control">
+          <label for="username">Username</label>
+          <input type="text" required name="username" />
+        </div>
+        <div className="form-control">
+          <label for="email">Email</label>
+          <input type="email" required name="email" />
+        </div>
+        <div className="form-control">
+          <label for="password">Password</label>
+          <input type="password" required name="password" />
+        </div>
+        <button type="submit" className="btn">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
