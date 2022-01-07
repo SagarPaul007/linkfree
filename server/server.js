b1e17d931b161d83101eb8f55730042b9d2131f1
YOUR NAME <sagarpaul2k16@gmail.com>
2022-01-07 05:53:21 +0530

    middlewares changed!

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -8,27 +7,21 @@ const app = express();
 const userRouter = require("./routers/user");
 const profileRouter = require("./routers/profile");
 
-// models
-const { User } = require("./models/user");
-
 // database connection
 mongoose.connect(
   "mongodb://localhost:27017/test",
   () => {
-    console.log("connected!");
+    console.log("connected to DB!");
   },
   (e) => console.log(e)
 );
 
-// middlewares
-app.use(bodyParser.json());
-app.use(userRouter);
-app.use(profileRouter);
+// Middlewares
+app.use(express.json());
 
-// User.create({ name: "John", age: 50 }, (err, res) => {
-//   if (err) throw err;
-//   console.log(res);
-// });
+// Routes
+app.use("/api", userRouter);
+app.use(profileRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("listening on port " + port));
